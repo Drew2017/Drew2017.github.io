c408b6659b62be19b1f87f0a8cfa888525374ae8
Drew2017 <andrew_mayerchak@hotmail.com>
2026-05-28 08:34:20 -0600

    Create Transaction.py

---
+++ Transaction.py
@@ -0,0 +1,12 @@
+import Date
+class lineItem:
+ 
+newArray=[]
+newTransaction = lineItem
+isDone = False
+while isDone==False:
+    newTransaction.date = Date.today
+    newTransaction.amount = float(input("Amount?"))
+    
+    getButtonPress
+    newTransaction.localCategory = pygui.butto
